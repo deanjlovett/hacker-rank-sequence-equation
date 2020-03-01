216a9ded920f1c22809a2a1d8dc6701256809a82
Dean Lovett <dean.lovett@gmail.com>
2020-02-29 23:42:12 -0800

    adds initial files

---
+++ se.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the permutationEquation function below.
+function permutationEquation(p) {
+    let rm = p.reduce((a,e,i)=> {a[e]=i+1;return a;} ,{});
+    let ret = p.map((e,i)=>rm[rm[i+1]]);
+    return ret;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const p = readLine().split(' ').map(pTemp => parseInt(pTemp, 10));
+
+    let result = permutationEquation(p);
+
+    ws.write(result.join("\n") + "\n");
+
+    ws.end();
+}
